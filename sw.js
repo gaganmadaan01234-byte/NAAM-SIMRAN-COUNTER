f36edb926e00248d69fa73a69d147b8827009154
gaganmadaan01234-byte <gaganmadaan01234@gmail.com>
2026-08-25 03:43:10 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE="naam-jap-v1";const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const C="waheguru-jap-v3";const A=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
